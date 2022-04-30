7f95051ff880c6c706cdd06230545ea82ebd0031
Thoumelin Armel <armel.thoumelin@gmail.com>
2022-04-30 17:25:54 +0200

    Console application implementation

---
+++ Application.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Application.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var operation = new BankOperation();
+                await operation.Start();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine(ConsoleResource.GeneralException);
+            }
+        }
+    }
+}
